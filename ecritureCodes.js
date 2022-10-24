78b5cc52ac6a82bea9c88d576981e3a2c67941db
Miguel989 <113317801+Miguel989@users.noreply.github.com>
2022-10-24 13:35:11 -0400

    ecriture du body des 8 types de donnees souhaitees

--- ecritureCodes.js
+++ ecritureCodes.js
@@ -32,6 +32,64 @@ Q2: Le nombres de voies par direction
   >;
 );
  out body;
+// Pour l'analyse, on veut "<tag k="crossing" v="unmarked"/>" pour voir le type de cycleway qu'on veut. Ensuite, on veut voir s'il y a un cycleway de present. 
+
+Q4: Superficie (en km2 et en %) :
+[bbox: 45.3755, -73.9979, 45.3920, -73.9748];
+(
+  relation ->.rel;
+  .rel map_to_area ->.usagesol;
+ 
+  nwr["landuse"](area.usagesol);
+  >;
+  nwr["amenity"](area.usagesol);
+  >;
+);
+ out body;
+
+
+ Q5: Analyse de l'offre du transport en commun
+
+ [bbox: 45.3755, -73.9979, 45.3920, -73.9748];
+(
+  node["public_transport"];
+  node["crossings"];
+  >;
+);
+ out body;
+// comment veut-on quantifier/analyser ces donnees? On les compar avec quoi? 
+
+
+Q6: Limite de vitesse maximale: 
+
+[bbox: 45.3755, -73.9979, 45.3920, -73.9748];
+(
+  way["maxspeed"];
+  >;
+  
+);
+ out body;
+
+ Q7: COS en moyen des batiments
+
+ [bbox: 45.3755, -73.9979, 45.3920, -73.9748];
+(
+
+  nwr["landuse"="residential"]; // ca va me ressortir le nombre d'unites dans les tags
+  >;
+
+);
+ out body;
+ // Est-ce qu'on peut prendre les aires pour chaque et de diviser le nombre de flats par l'aire totale? Et ensuite prendre la moyenne? - Miguel 24-10
+
+Q8: Types de commerces
+
+[bbox: 45.3755, -73.9979, 45.3920, -73.9748];
+(
+  node["shop"];
+  >;
+);
+ out body;
 
 */
 
