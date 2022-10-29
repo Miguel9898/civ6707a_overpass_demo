c96f56f847c83dea9126143dfc3c59cc13ac6843
Miguel989 <113317801+Miguel989@users.noreply.github.com>
2022-10-29 12:27:19 -0400

    changement du fichier, meme probleme...

--- testImportGeojson.js
+++ testImportGeojson.js
@@ -27,25 +27,25 @@ const calculateLength = function(feature) {
     }
 };
 
-const geojson = readGeojsonFile('./export.geojson');
+const geojson = readGeojsonFile('./exportway.geojson');
 
 // au lieu d'un map on cree le foreach (qui le rempli au fur et a mesure pour chaque id (map ca cree un array.)
 
-const areaAndlengthbyId = {}; // on veut store nos valeurs dans cet objet vide. C
+const areaAndlengthbyId = {}; // on veut store nos valeurs dans cet objet vide. 
 geojson.features.forEach(function(feature) {
     const area = calculateArea(feature);
     const length = calculateLength(feature);
-    areaAndlengthbyId[feature.properties.id] = {
+    areaAndlengthbyId[feature.id] = {
         area: area,
         length: length
     };
 });
-   
+/*
 const nums = [1,2,3,4,5,6,7,8,9,10];
 nums.forEach(function(num) {
     console.log(num ** 2);
 });
-
+*/
 
 
 /* Ca nous calcule les aires et les longueurs, mais ca ne nous retourne pas les valeurs dans le format souhaite. 
